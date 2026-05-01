11c6270a186b5d226b3c46647aeeb3373a69f358
kmg <kang834130@gmail.com>
2026-05-01 23:01:37 +0900

    Add files via upload

---
+++ 파이썬 2~3장/5팀 2장 실습 3번.py
@@ -0,0 +1,8 @@
+n1 = int(input("첫번째 숫자 입력 : "))
+n2 = int(input("두번째 숫자 입력 : "))
+n3 = int(input("세번째 숫자 입력 : "))
+
+sum = n1+n2+n3
+avg = sum/3
+
+print("변수들의 합은 ",sum," 변수들의 평균은" ,avg) 
---
+++ 파이썬 2~3장/5팀 2장 실습 5번.py
@@ -0,0 +1,17 @@
+import turtle as t
+t.shape("turtle")
+radius = 50
+t.up()
+t.goto(0,0)
+t.down()
+t.circle(radius)
+t.up()
+t.goto(100,0)
+t.down()
+radius+=20
+t.circle(radius)
+t.up()
+t.goto(200,0)
+t.down()
+radius+=20
+t.circle(radius)
